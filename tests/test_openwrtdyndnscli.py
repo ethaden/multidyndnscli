84e65f0815c6277e0084ef33d969bec8c1a9176d
Eike Thaden <eike.thaden@gmail.com>
2023-03-31 17:01:03 +0200

    Testing for inavlid ips

--- tests/test_openwrtdyndnscli.py
+++ tests/test_openwrtdyndnscli.py
@@ -2,6 +2,11 @@ import pytest
 import netaddr
 import openwrtdyndnscli
 
+testdata_invalid_ip = [
+    ('1.2.3.4.5'),
+    (':abcde::')
+]
+
 testdata_ipv4 = [
     ('192.168.0.1', False),
     ('192.168.25.23', False),
@@ -15,14 +20,20 @@ testdata_ipv6 = [
     ('fc00::aaaa:bbbb:cccc:dddd', False),
     ('2a02:8000:a000:f000:ffff:ffff:ffff:ffff', True),
     ('2a02:8000:a000:f000:0:0:0:0', True),
-    ('2a02:8000:a000:f000:0:0:0:1', True),
+    ('2a02:8000:a000:f000:0:0:0:1', True)
 ]
 
+@pytest.mark.parametrize("address", testdata_invalid_ip)
+def test_get_exception_for_invalid_ip(address):
+    with pytest.raises(Exception):
+        openwrtdyndnscli.get_valid_ip(address)
 
 @pytest.mark.parametrize("address, expected", testdata_ipv4)
 def test_is_public_ipv4(address, expected):
-    assert openwrtdyndnscli.is_public_ipv4(address) == expected
+    addr = openwrtdyndnscli.get_valid_ip(address)
+    assert openwrtdyndnscli.is_public_ipv4(addr) == expected
 
 @pytest.mark.parametrize("address, expected", testdata_ipv6)
 def test_is_public_ipv6(address, expected):
-    assert openwrtdyndnscli.is_public_ipv6(address) == expected
+    addr = openwrtdyndnscli.get_valid_ip(address)
+    assert openwrtdyndnscli.is_public_ipv6(addr) == expected
