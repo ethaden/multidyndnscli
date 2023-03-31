0fe2e37f093b821e3640bfb73f683c72da856316
Eike Thaden <eike.thaden@gmail.com>
2023-03-31 17:06:23 +0200

    type hints and IPv4 resolution

--- openwrtdyndnscli/__init__.py
+++ openwrtdyndnscli/__init__.py
@@ -1,11 +1,12 @@
 import sys
 import argparse
 import openwrtdyndnscli
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 import yaml
 import dns.resolver
 import netaddr
 import netifaces
+import requests
 
 ipv4_private_net_192_168 = netaddr.IPNetwork('192.168.0.0/16')
 ipv4_private_net_172_16 = netaddr.IPNetwork('172.16.0.0/12')
@@ -14,24 +15,28 @@ ipv6_private_net_fc = netaddr.IPNetwork('fc00::/7') # Unique Local Addresses (UL
 ipv6_private_net_fd = netaddr.IPNetwork('fd00::/8') # Management addresses
 ipv6_private_net_fe = netaddr.IPNetwork('fe80::/10') # Addresses used for autoconfiguration
 
-def is_public_ipv4(address: str) -> bool:
+def get_valid_ip(address: str) -> netaddr.IPAddress:
     addr = netaddr.IPAddress(address)
-    return not((addr in ipv4_private_net_10) or (addr in ipv4_private_net_172_16) or (addr in ipv4_private_net_192_168))
+    return addr
 
-def get_ipv4_addresses_linux(interface: str, public_only: bool=True) -> List[str]:
+
+def is_public_ipv4(address: netaddr.IPAddress) -> bool:
+    return not((address in ipv4_private_net_10) or (address in ipv4_private_net_172_16) or (address in ipv4_private_net_192_168))
+
+def get_ipv4_addresses_linux(interface: str, public_only: bool=True) -> List[netaddr.IPAddress]:
     """
     Find all IPv6 addresses of the given interfaces.
     """
 
     addrs = netifaces.ifaddresses(interface)
-    addrs_list = [addr['addr'] for addr in addrs[netifaces.AF_INET]]
+    address_string_list = [addr['addr'] for addr in addrs[netifaces.AF_INET]]
+    address_list = [ get_valid_ip(address) for address in address_string_list]
     if public_only:
-        return [ addr for addr in addrs_list if is_public_ipv4(addr)]
-    return [ addr['addr'] for addr in addrs[netifaces.AF_INET]]
+        return [ addr for addr in address_list if is_public_ipv4(addr)]
+    return address_list
 
-def is_public_ipv6(address: str) -> bool:
-    addr = netaddr.IPAddress(address)
-    return not((addr in ipv6_private_net_fc) or (addr in ipv6_private_net_fd) or (addr in ipv6_private_net_fe))
+def is_public_ipv6(address: netaddr.IPAddress) -> bool:
+    return not((address in ipv6_private_net_fc) or (address in ipv6_private_net_fd) or (address in ipv6_private_net_fe))
 
 def get_ipv6_addresses_linux(interface: str, public_only: bool=True) -> List[str]:
     """
@@ -40,17 +45,18 @@ def get_ipv6_addresses_linux(interface: str, public_only: bool=True) -> List[str
 
     addrs = netifaces.ifaddresses(interface)
     # Note, that addresses used for autoconfiguration have the format "ipv6_adddr%interface_name"
-    addrs_list = [addr['addr'].split('%')[0] for addr in addrs[netifaces.AF_INET6]]
+    address_string_list = [addr['addr'].split('%')[0] for addr in addrs[netifaces.AF_INET6]]
+    addresses_list = [ get_valid_ip(address) for address in address_string_list]
     if public_only:
-        return [ addr for addr in addrs_list if is_public_ipv6(addr)]
-    return [ addr['addr'] for addr in addrs[netifaces.AF_INET6]]
+        return [ addr for addr in addresses_list if is_public_ipv6(addr)]
+    return addresses_list
 
-def get_public_ipv6(hostname: str) -> str:
-    result = dns.resolver.query(hostname, 'AAAA')
+def get_public_ipv6(hostname: str) -> netaddr.IPAddress:
+    result_list = dns.resolver.query(hostname, 'AAAA')
     # Remove local ones
-    for val in result:
+    for val in result_list:
         if not val.address.startswith('fd') and not val.address.startswith('fe') and not val.address.startswith('fc'):
-            return val.address
+            return get_valid_ip(val.address)
     return None
 
 def update_domain(netcup_config, router_config, domain_config):
@@ -58,12 +64,18 @@ def update_domain(netcup_config, router_config, domain_config):
     
     print (domain_config)
 
-class RouterAddresses:
+class RouterConfig:
 
-    def _get_public_ipv4(self, ipv4_config) -> str:
+    def _get_public_ipv4(self, ipv4_config) -> netaddr.IPAddress:
         if ipv4_config['method'] == 'web':
             url = ipv4_config['url']
             #print ('Using URL to get external IPv4: {url}')
+            response = requests.get(url)
+            if response:
+                ipv4_candidate = response.text
+                return get_valid_ip(ipv4_candidate)
+            else:
+                raise Exception(f'Unable to determine external IPv4 of router through website {url}')
         elif ipv4_config['method'] == 'wan':
             wan_interface = ipv4_config['wan_interface']
             #print ('Using wan interface as source for IPv4: {wan_interface}')
@@ -84,10 +96,10 @@ class RouterAddresses:
             return None if len(ipv6_list)==0 else ipv6_list[0]
         return None
 
-    def __init__(self, config):
-        router_public_address_config = config['router_public_address']
-        self.use_ipv4 = config.get('use_ipv4', False)
-        self.use_ipv6 = config.get('use_ipv6', False)
+    def __init__(self, config_dict):
+        router_public_address_config = config_dict['router_public_address']
+        self.use_ipv4 = config_dict.get('use_ipv4', False)
+        self.use_ipv6 = config_dict.get('use_ipv6', False)
         self.ipv4 = None
         self.ipv6 = None
         self.wan_interface_ipv4 = None
@@ -101,17 +113,55 @@ class RouterAddresses:
         
 
 class NetcupConfig:
-    def __init__(self, config):
-        self.netcup_userid = config['netcup_userid']
-        self.netcup_apikey = config['netcup_apikey']
-        self.netcup_apipass = config['netcup_apipass']
+    def __init__(self, netcup_config_dict):
+        self.netcup_userid = netcup_config_dict['netcup_userid']
+        self.netcup_apikey = netcup_config_dict['netcup_apikey']
+        self.netcup_apipass = netcup_config_dict['netcup_apipass']
 
-def update(config)->int:
-    netcup_config = NetcupConfig(config)
-    router_config = RouterAddresses(config)
-    domain_config_list = config['domains']
-    for domain_config in domain_config_list:
-        update_domain(netcup_config, router_config, domain_config)
+class HostConfig:
+    def __init__(self, domain_config, host_config_dict):
+        self.domain_config = domain_config
+        self.name = host_config_dict['name']
+        self.fqdn = host_config_dict['fqdn']
+        public_ip_methods = host_config_dict.get('public_ip_methods', None)
+        if public_ip_methods is not None:
+            self.public_ip_method_ipv4 = public_ip_methods.get('ipv4')
+            self.public_ip_method_ipv6 = public_ip_methods.get('ipv4')
+        print (f'Reading settings for hostname {self.name}')
+
+    def is_up_to_date(self) -> bool:
+        pass
+
+    def get_record_ipv4(self):
+        pass
+
+class DomainConfig:
+    host_configs = []
+
+    def _get_host_settings(self, config_host_dict):
+        pass
+        
+    def __init__(self, router_config, netcup_config, domain_config_dict):
+        self.router_config = router_config
+        self.netcup_config = netcup_config
+        self.domain_name = domain_config_dict['name']
+        for host_config_dict in domain_config_dict['hosts']:
+            self.host_configs.append(HostConfig(self, host_config_dict))
+    
+    def get_router_config(self) -> RouterConfig:
+        return self.router_config
+
+    def update(self):
+        print (f'Updating addresses for domain "{self.domain_name}"')
+
+def update(config_dict)->int:
+    netcup_config = NetcupConfig(config_dict)
+    router_config = RouterConfig(config_dict)
+    domain_config_list = config_dict['domains']
+    for domain_config_dict in domain_config_list:
+        domain_config = DomainConfig(netcup_config, router_config, domain_config_dict)
+        domain_config.update()
+
     #print(get_ipv4_addresses_linux(config['wan_interface']))
     #print(get_ipv6_addresses_linux(config['wan_interface']))
     #print (get_public_ipv6('homeassistant.lan'))
