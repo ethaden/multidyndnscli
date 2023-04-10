310bce2539e9cd93aadc8ef82deca37d6f740e06
Eike Thaden <eike.thaden@gmail.com>
2023-04-10 14:58:58 +0200

    Progress validating input format

--- openwrtdyndnscli/schemata.py
+++ openwrtdyndnscli/schemata.py
@@ -1,8 +1,31 @@
-from schema import Schema, And, Or
+from schema import Schema, And, Or, Optional, Use
 
 def get_config_file_schema():
     return Schema({
-        "dns-providers": [{
-            "name": Or("Netcup")
-        }]
+        "dns-providers": [ Or({
+            "name": And(Use(str.lower), "netcup"),
+            "userid": And(str, len),
+            "apikey": And(str, len),
+            "apipass": And(str, len)
+        })
+        ],
+        "router": {
+            "ipv4": {
+                "enabled": bool,
+                "method": ["wan", "fritzbox"],
+                Optional("fritzbox"):
+                    {
+                        "address": And(str, Use(str.lower), 
+                            lambda x: x.startswith('http://') or x.startswith('https://'))
+                    }        
+            },
+            "ipv6": {
+                "enabled": bool,
+                "method": ["wan", "fritzbox"],
+                Optional("fritzbox"):
+                    {
+                        "address": And(str, Use(str.lower), lambda x: x.startswith('http://') or x.startswith('https://'))
+                    }
+            }
+        }
     })
